a5a8454709cff8c95185e95a33bc8b1516b2c160
ODEN-SUKI <38833158+ODEN-SUKI@users.noreply.github.com>
2018-05-31 17:42:55 +0900

    Update travis_test_buzzer.py

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -8,7 +8,7 @@ from std_msgs.msg import UInt16
 class BuzzerTest(unittest.TestCase):
 	def test_node_exist(self):
 		nodes = rosnode.get_node_names()
-		self.assertIn('/buzzer',nodes,"node does not exit")
+		self.assertIn('/buzzer', nodes, "node does not exit")
 
         def test_put_value(self):
 		pub = rospy.Publisher('/buzzer', UInt16)
@@ -18,7 +18,7 @@ class BuzzerTest(unittest.TestCase):
 			
 	with open("/dev/rtbuzzer0","r") as f:
 			data = f.readline()
-			self.assertEqual(data,"1234\n","value does not written to rtbuzzer0")
+			self.assertEqual(data, "1234\n", "value does not written to rtbuzzer0")
 			
 		
 		
